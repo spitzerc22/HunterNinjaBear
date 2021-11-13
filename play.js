9bcede4dad55964fc15290318acb9cbd070f20d1
spitzerc22 <spitzerc22@gmail.com>
2021-11-13 14:29:52 -0600

    computer choice is randomnized

--- play.js
+++ play.js
@@ -1,22 +1,26 @@
 //event listeners options//
 const playAgain = document.querySelector(".playAgain")
 const choices = document.querySelectorAll(".img")
-
+let yourScore = document.querySelector(".score")
+let compScore = document.querySelector(".comscore")
 
 choices.forEach(choice => choice.addEventListener('click', () => {
-  
+  let userChoice = choice.alt
   const cards = document.querySelector(".cards")
-  const whoWins = document.querySelector(".whoWins")
   cards.style.display = "none";
+  const whoWins = document.querySelector(".whoWins")
   whoWins.style.display = "flex";
-  
-  document.getElementById("userPickImage").src = choice.src
-  
+  console.log(choices)
+  document.getElementById("userPick").src = choice.src
+
   let computerChoice = getComputerChoice();
-    //displays who won//
-    let whoWins = (whoWins(userChoice, computerChoice))
-    let winner = document.getElementById("#winner")
-    winner.textcontent = whoWins;
+  if(computerChoice === 'hunter') {
+    document.getElementById("computerPick").src = "assets/hunter.face.png"
+  } else if(computerChoice === 'ninja') {
+    document.getElementById("computerPick").src = "assets/ninja.face.png"
+  } else {document.getElementById("computerPick").src = "assets/bear.face.png"}
+
+  whoWins(userChoice, computerChoice)
 }))
 
 
@@ -53,8 +57,7 @@ playAgain.addEventListener('click', () => {
   
 //comparison, who wins each round// 
 
-let yourScore = document.querySelector(".score")
-let compScore = document.querySelector(".comscore")
+
 
   let whoWins = (userChoice, computerChoice) => {
     if(userChoice === computerChoice){
@@ -85,13 +88,13 @@ let compScore = document.querySelector(".comscore")
   
 //calling to play the game//
 
-  let playGame = () => { //run with event
-    let userChoice = getUserChoice();
-    let computerChoice = getComputerChoice();
-    console.log('The computer threw ' + computerChoice)
-    console.log('You threw ' + userChoice)
-    //displays who won//
-    let whoWins = (whoWins(userChoice, computerChoice))
-    let winner = document.getElementById("#winner")
-    winner.textcontent = whoWins;
-  }
+  // let playGame = () => { //run with event
+  //   let userChoice = getUserChoice();
+  //   let computerChoice = getComputerChoice();
+  //   console.log('The computer threw ' + computerChoice)
+  //   console.log('You threw ' + userChoice)
+  //   //displays who won//
+  //   let whoWins = (whoWins(userChoice, computerChoice))
+  //   let winner = document.getElementById("#winner")
+  //   winner.textcontent = whoWins;
+  // }
