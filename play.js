9fc34665104be262cf3d3bc4652e8813646d6609
spitzerc22 <spitzerc22@gmail.com>
2021-11-14 17:15:43 -0600

    changed css, added modal, function to display winner(but keeps appending, does not replace)

--- play.js
+++ play.js
@@ -1,16 +1,42 @@
-//event listeners options//
+
 const playAgain = document.querySelector(".playAgain")
 const choices = document.querySelectorAll(".img")
-let yourScore = document.querySelector(".score")
-let compScore = document.querySelector(".comscore")
+let yourScore = document.getElementById("score")
+let compScore = document.getElementById("comscore")
+let finalWinner = document.getElementById("winner")
+
+//Comparing your choice and computer choice to find winner//
+
+let whoWon = (userChoice, computerChoice) => {
+  if(userChoice === computerChoice){
+  return('It\'s a tie!')
+} else if(userChoice === 'hunter') {
+  if(computerChoice === 'ninja') {
+    return('The computer wins!')
+  } else {
+    return('You win!')}
+} else if(userChoice === 'ninja'){
+  if(computerChoice === 'bear'){
+    return('The computer wins!')
+  } else {
+    return('You win!')}
+} else if(userChoice === 'bear'){
+  if(computerChoice === 'hunter'){
+    return('The computer wins!')
+  } else {
+    return('You win!')}
+}
+}
+
+//Picking a choice, displaying it and running whoWon()//
 
 choices.forEach(choice => choice.addEventListener('click', () => {
+ 
   let userChoice = choice.alt
   const cards = document.querySelector(".cards")
   cards.style.display = "none";
   const whoWins = document.querySelector(".whoWins")
   whoWins.style.display = "flex";
-  console.log(choices)
   document.getElementById("userPick").src = choice.src
 
   let computerChoice = getComputerChoice();
@@ -20,10 +46,37 @@ choices.forEach(choice => choice.addEventListener('click', () => {
     document.getElementById("computerPick").src = "assets/ninja.face.png"
   } else {document.getElementById("computerPick").src = "assets/bear.face.png"}
 
-  whoWins(userChoice, computerChoice)
+  
+  let comparison = whoWon(userChoice, computerChoice);
+  
+  if(comparison === "You win!"){
+    finalWinner.append(comparison)
+  } else if(comparison === "The computer wins!") {
+    finalWinner.append(comparison)
+  } else {finalWinner.append(comparison)}
+  
+  
+ 
+  
+  
 }))
 
-
+//getting computer choice// 
+  
+let getComputerChoice = () => {
+  let choice = Math.floor(Math.random()*3)
+  switch(choice){
+    case 0:
+      return 'hunter'
+      break;
+    case 1:
+      return 'ninja'
+      break;
+    case 2:
+      return 'bear'
+      break;
+  }
+}
 
 //Play Again button//
 
@@ -33,68 +86,3 @@ playAgain.addEventListener('click', () => {
   const cards = document.querySelector(".cards")
   cards.style.display = "flex";
 })
-
-
-
-
-  
-//getting computer choice// 
-  
-  let getComputerChoice = () => {
-    let choice = Math.floor(Math.random()*3)
-    switch(choice){
-      case 0:
-        return 'hunter'
-        break;
-      case 1:
-        return 'ninja'
-        break;
-      case 2:
-        return 'bear'
-        break;
-    }
-  }
-  
-//comparison, who wins each round// 
-
-
-
-  let whoWins = (userChoice, computerChoice) => {
-    if(userChoice === computerChoice){
-    return('It\'s a tie!')
-  } else if(userChoice === 'hunter') {
-    if(computerChoice === 'ninja') {
-      compScore += 1;
-      return('The computer wins!')
-    } else {
-      yourScore += 1;
-      return('You win!')}
-  } else if(userChoice === 'ninja'){
-    if(computerChoice === 'bear'){
-      compScore += 1;
-      return('The computer wins!')
-    } else {
-      yourScore += 1;
-      return('You win!')}
-  } else if(userChoice === 'bear'){
-    if(computerChoice === 'hunter'){
-      compScore += 1;
-      return('The computer wins!')
-    } else {
-      yourScore += 1;
-      return('You win!')}
-  }
-  }
-  
-//calling to play the game//
-
-  // let playGame = () => { //run with event
-  //   let userChoice = getUserChoice();
-  //   let computerChoice = getComputerChoice();
-  //   console.log('The computer threw ' + computerChoice)
-  //   console.log('You threw ' + userChoice)
-  //   //displays who won//
-  //   let whoWins = (whoWins(userChoice, computerChoice))
-  //   let winner = document.getElementById("#winner")
-  //   winner.textcontent = whoWins;
-  // }
